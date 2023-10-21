7575e44233238fae264455be85a15721367d2ad8
Umidjon <147255061+Umidjonjuraqulov@users.noreply.github.com>
2023-10-21 21:18:00 +0500

    Add files via upload

---
+++ Bosh_sahifa.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+col1,col2,col3 = st.columns((1, 2, 7))
+col3.title(" Xush kelibsiz ! ")
+col1.image("images/python_logo.png")
+col2.write("")
+
+st.subheader("Python darslar")
+st.write("Assalomu alakum ! **Python dasturlash tili bo'yicha darslar** saytimizda sizni ko'rganimizdan  xursandmiz.Bu sayt orqali python bo'yicha o'z bilim va ko'nikmalaringizni ortirib borishingiz mumkin .Bu saytda  mantiqiy fikrlashni oshiruvchi mashqlar va ularning python dasturlash tilidagi javoblarini topishingiz mumkin.")
+
+col1,col2 = st.columns((1, 9))
+col1.image("images/leetcode_logo.png")
+col2.subheader("LeetCode")
+st.write("Eng ko'p yoqtirilgan LeetCode masalalar va ularning yechimlarini biz bilan o'rganing .")
+st.image("images/bosh_sahifa.png")
+
+st.write("**Darslar soni**: ")
+col1,col2 = st.columns((2,  11))
+col1.write("**Tuzuvchilar**: ")
+col2.write("**Firuz Juraev** ")
+col2.write("[**Umidjon Juraqulov**](https://umidjon.streamlit.app/)")
