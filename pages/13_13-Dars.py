6ff2c92a84189b928acf974bda113d6a4ee0cf7b
Umidjon <147255061+Umidjonjuraqulov@users.noreply.github.com>
2023-10-29 11:39:20 +0500

    Update 13_13-Dars.py

--- pages/13_13-Dars.py
+++ pages/13_13-Dars.py
@@ -4,7 +4,7 @@ import streamlit as st
 savol=("Kiritilgan raqamlar orasida 1 raqami necha marta takrorlanganini chiqaruvchi dastur tuzing.")#savol matnini kiriting
 
 col1,col2 = st.columns((1, 7))
-col1.image("images/leetcode_logo.png")
+col1.image("images/leetcode_logo.PNG")
 col2.write(f"**Savol**.{savol} ")
 
 st.write("**Yeshim**:")
